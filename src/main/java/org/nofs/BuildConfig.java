37862f3c1527ed24dc5e54c754e60243092d005e
lxzqaq <2982029868@qq.com>
2023-07-15 15:24:28 +0800

    ?

--- src/main/java/org/nofs/BuildConfig.java
+++ src/main/java/org/nofs/BuildConfig.java
@@ -2,5 +2,5 @@ package org.nofs;
 
 public final class BuildConfig {
     public static final String VERSION = "3.2";
-    public static final String GIT_HASH = "01ce6a9";
+    public static final String GIT_HASH = "";
 }
